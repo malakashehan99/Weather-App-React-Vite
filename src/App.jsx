5df5469ed56977caaec355535b4929b996d05443
Malaka Shehan <120486113+malakashehan99@users.noreply.github.com>
2024-06-28 21:44:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,17 @@ import Login from './components/Login';
 
 
 function App() {
-  // State to track whether the user is logged in or not
+  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  // Function to handle successful login
+  
   const handleSuccessfulLogin = () => {
     setIsLoggedIn(true);
   };
 
   return (
     <div className="app">
-      {/* 
-        Render the login component if the user is not logged in (isLoggedIn is false),
-        otherwise render the weather component
-      */}
+      
       {!isLoggedIn ? <Login onLogin={handleSuccessfulLogin} /> : <Weather />}
     </div>
   );
